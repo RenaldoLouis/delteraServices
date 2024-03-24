ce429d7dd834c4460d86daa920d388c7520c4620
Renaldo Louis <renaldo.louis@commsult.id>
2024-03-24 10:44:31 +0700

    fix autoIncrement id users

--- migrations/sqls/20240324025605-initialize-up.sql
+++ migrations/sqls/20240324025605-initialize-up.sql
@@ -1,8 +1,10 @@
 -- Table: public.Users
 
+CREATE SEQUENCE users_id_seq;
+
 CREATE TABLE public."users"
 (
-    id bigint,
+    id bigint DEFAULT nextval('users_id_seq'::regclass) PRIMARY KEY,
     name character varying COLLATE pg_catalog."default",
     email character varying COLLATE pg_catalog."default"
 )
